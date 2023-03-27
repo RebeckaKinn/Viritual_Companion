296fbbfc98fb83b8e4aca47f79014863472edf7f
RebeckaKinn <rebeckaki@getacademy.no>
2023-03-27 12:50:26 +0200

    Create Pet.cs

---
+++ Pet.cs
@@ -0,0 +1,27 @@
+﻿namespace Viritual_Companion
+{
+    public class Pet
+    {
+        private string Name { get; set; }
+        private string Type { get; set; }
+        private int Life { get; set; }
+
+        public Pet(string name)
+        {
+            Name = name;
+        }
+        public Pet(string name, string type)
+        {
+            Name = name;
+            Type = type;
+        }
+        public Pet(string name, string type, int life)
+        {
+            Name = name;
+            Type = type;
+            Life = life;
+        }
+    }
+
+
+}
